978dc124a5f6042d27146cd057f7c161a0320731
jossif eleftteriadis <jossif.elefteriadis@appliedtechnology.se>
2019-11-21 13:38:34 +0100

    add proper redirections for waiting room

--- musicquiz/src/components/Playlists.js
+++ musicquiz/src/components/Playlists.js
@@ -1,64 +1,83 @@
 import React from 'react'
+import { withRouter } from 'react-router';
+
 
 class Playlists extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             myPlaylists: [],
             access_token: '',
             selectedPlaylistId: ''
-          }
+        }
     }
     getAllPlaylists = () => {
         const access_token = this.props.access_token
         // const refresh_token = window.location.hash.split('refresh_token=')[1];
         fetch(`http://localhost:5000/playlist?token=${access_token}`
-          // , {
-          // headers: {
-          //   "Accept": "application/json",
-          //   "Content-Type": "application/json",
-          //   "Authorization": 'Bearer ' + access_token
-          // }
-          // }
+            // , {
+            // headers: {
+            //   "Accept": "application/json",
+            //   "Content-Type": "application/json",
+            //   "Authorization": 'Bearer ' + access_token
+            // }
+            // }
         )
-          .then(response => response.json())
-          .then(data => {
-            console.log('get all playlists', data)
-            this.setState({ myPlaylists: data, access_token: access_token })
-          });
-      }
-      getSpecificPlaylistId = (id) => {
+            .then(response => response.json())
+            .then(data => {
+                console.log('get all playlists', data)
+                this.setState({ myPlaylists: data, access_token: access_token })
+            });
+    }
+    getSpecificPlaylistId = (id) => {
         const access_token = this.props.access_token
         this.setState({ selectedPlaylistId: id })
         fetch(`http://localhost:5000/random/${id}?token=${access_token}`)
-        .then(response => response.json())
-          .then(data => { 
-              console.log('from random endpoint', data)
-          })
-          .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(data => {
+                console.log('from random endpoint', data)
+            })
+            .catch(error => console.log(error))
 
-      }
 
-      componentDidMount() {
+    }
+
+    connectToRoom = () => {
+        this.props.history.push('/Waitingroom')
+    }
+
+    componentDidMount() {
         // const access_token = window.location.hash.split('=')[1].split('&')[0];
         // this.props.setToken(access_token)
         this.getAllPlaylists()
     }
-    
+
     render() {
-    return (
-        <div>
-             {/* <button on={this.getAllPlaylists}>Show all playlists</button> */}
-             {this.state.myPlaylists.map((item, index) => {
-                 
-            return <div key={index}>
-              <p className={ item.id === this.state.selectedPlaylistId ?
-                     'selected_playlist'
-                     : ''} onClick={() => this.getSpecificPlaylistId(item.id)}>{item.name}</p>
+        return (
+            <div>
+                {/* <button on={this.getAllPlaylists}>Show all playlists</button> */}
+                {this.state.myPlaylists.map((item, index) => {
+
+                    return <div key={index}>
+                        <p className={item.id === this.state.selectedPlaylistId
+                            ? 'selected_playlist'
+                            : ''}
+                            onClick={() => this.getSpecificPlaylistId(item.id)}>
+                            {item.name}
+                        </p>
+                    </div>
+                })}
+                <button className="selected_playlist_btn"
+                    type="submit"
+                    onClick={this.connectToRoom}
+                    style={this.state.selectedPlaylistId === ''
+                        ? { visibility: "hidden" }
+                        : { visibility: "visible" }}>
+                    Next(Waiting room)
+                </button>
             </div>
-          })}
-        </div>
-    )
+        )
     }
 }
-export default Playlists;
+
+export default withRouter(Playlists);
